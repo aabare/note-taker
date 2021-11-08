0c94c1808c382e6dd83e87a395c0f4e1010a6439
Adam <a.6942@yahoo.com>
2021-11-07 21:37:53 -0500

    changed port

--- servers.js
+++ servers.js
@@ -11,6 +11,6 @@ app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+app.listen(PORT, () => {
+    console.log('API server now on port ${PORT}');
   });
